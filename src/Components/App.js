55a0d62551b3bb266e4b8bdc47695bddea1f18eb
Rad <chihaby@gmail.com>
2019-07-17 23:34:10 -0700

    sets up components

---
+++ src/Components/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import axios from 'axios'
+import SearchBar from './SearchBar' 
+import LabelList from './LabelList'
+
+class App extends React.Component {
+    state = { images: [] };
+
+    loadAllBreeds = async () => {
+    await axios.get('https://dog.ceo/api/breeds/list/all')
+    .then(response => { 
+            const labels = Object.keys(response.data.message).slice(0, 12);
+            console.log(labels);
+            this.setState({images: labels});
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+}
+
+    render () {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.loadAllBreeds} />
+                <LabelList images={this.state.images}/>
+            </div>
+        );
+    };
+}
+
+export default App;
---
+++ src/Components/LabelList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LabelList = props => {
+    console.log(props.images)
+    return <div>Button</div>
+};
+
+export default LabelList;
