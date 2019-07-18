e9ed6445ed3162122a2aeb10ead51dfb41117771
Rad <chihaby@gmail.com>
2019-07-18 00:47:54 -0700

    renders the first 12 breed buttons

--- src/Components/LabelList.js
+++ src/Components/LabelList.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+loadImages = () => {
+    console.log('yes loading');
+}
+
 const LabelList = props => {
-    console.log(props.images)
-    return <div>Button</div>
+    const labels = props.images.map(image => {
+        return <button 
+                    key={image.id} 
+                    class="ui teal basic button"
+                    onClick={loadImages}
+                >
+                    {image}
+                </button>
+    });
+    return <div>{labels}</div>
 };
 
 export default LabelList;
